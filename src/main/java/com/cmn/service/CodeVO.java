e6deef26b09f7b72c2c02c722fae49860b8bf79c
silver-sh96 <cubha@naver.com>
2023-01-30 14:25:08 +0900

    esh test

--- src/main/java/com/cmn/service/CodeVO.java
+++ src/main/java/com/cmn/service/CodeVO.java
@@ -1,50 +1,39 @@
 package com.cmn.service;
 
 public class CodeVO {
-	private String COMMON_CODE;
-	private String UPPER_COMMON_CODE;
-	private String CODE_NAME;
-	private String CODE_DESCRIPTION;
-	private String CODE_REG_DATE;
-	private String CODE_MODIFY_DATE;
+	private String commonCode;
+	private String upperCommonCode;
+	private String codeName;
+	private String codeDescription;
 	
-	
-	public String getCOMMON_CODE() {
-		return COMMON_CODE;
+	public String getCommonCode() {
+		return commonCode;
 	}
-	public void setCOMMON_CODE(String cOMMON_CODE) {
-		COMMON_CODE = cOMMON_CODE;
+	public void setCommonCode(String commonCode) {
+		this.commonCode = commonCode;
 	}
-	public String getUPPER_COMMON_CODE() {
-		return UPPER_COMMON_CODE;
+	public String getUpperCommonCode() {
+		return upperCommonCode;
 	}
-	public void setUPPER_COMMON_CODE(String uPPER_COMMON_CODE) {
-		UPPER_COMMON_CODE = uPPER_COMMON_CODE;
+	public void setUpperCommonCode(String upperCommonCode) {
+		this.upperCommonCode = upperCommonCode;
 	}
-	public String getCODE_NAME() {
-		return CODE_NAME;
+	public String getCodeName() {
+		return codeName;
 	}
-	public void setCODE_NAME(String cODE_NAME) {
-		CODE_NAME = cODE_NAME;
+	public void setCodeName(String codeName) {
+		this.codeName = codeName;
 	}
-	public String getCODE_DESCRIPTION() {
-		return CODE_DESCRIPTION;
+	public String getCodeDescription() {
+		return codeDescription;
 	}
-	public void setCODE_DESCRIPTION(String cODE_DESCRIPTION) {
-		CODE_DESCRIPTION = cODE_DESCRIPTION;
-	}
-	public String getCODE_REG_DATE() {
-		return CODE_REG_DATE;
-	}
-	public void setCODE_REG_DATE(String cODE_REG_DATE) {
-		CODE_REG_DATE = cODE_REG_DATE;
-	}
-	public String getCODE_MODIFY_DATE() {
-		return CODE_MODIFY_DATE;
-	}
-	public void setCODE_MODIFY_DATE(String cODE_MODIFY_DATE) {
-		CODE_MODIFY_DATE = cODE_MODIFY_DATE;
+	public void setCodeDescription(String codeDescription) {
+		this.codeDescription = codeDescription;
 	}
 	
-	
+	@Override
+	public String toString() {
+		return "CodeVO [commonCode=" + commonCode + ", upperCommonCode=" + upperCommonCode + ", codeName=" + codeName
+				+ ", codeDescription=" + codeDescription + "]";
+	}
 }
